6a5ba29f1b1ff5b5c9d5f9128ae1505b144e48cb
doovers <dooversoft@gmail.com>
2016-10-18 16:18:34 +1100

    Initial Commit

---
+++ FirebaseMock/FirebaseMock.h
@@ -0,0 +1,19 @@
+//
+//  FirebaseMock.h
+//  FirebaseMock
+//
+//  Created by Colm Du Ve on 18/10/2016.
+//  Copyright © 2016 dooverSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirebaseMock.
+FOUNDATION_EXPORT double FirebaseMockVersionNumber;
+
+//! Project version string for FirebaseMock.
+FOUNDATION_EXPORT const unsigned char FirebaseMockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseMock/PublicHeader.h>
+
+
